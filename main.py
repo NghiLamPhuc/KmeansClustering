f8c3523882bd3da4378b71453c84d6089b4acca8
lamphucnghi313 <lamphucnghi@gmail.com>
2019-11-11 15:32:12 +0700

    Đáng lẽ hàm predict phải nằm ở Kmeasn class, mà tại mỗi lần lặp tính center distance các kiểu á, thì ghi file luôn. Nên chưa thêm hàm predict bên Kmeans class được á, nên tạm ghi bên đây.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Point import Point
 from Kmeans import Kmeans
-from read_file import read_lines_to_list
+from read_file import read_lines_to_list, read_input_file_to_int
 
 ## K-means su dung input khong gan nhan (non label) 
 ##
@@ -22,11 +22,33 @@ def set_initData_with_labeledData(dir: str, fileName: str, splitType: str) -> li
         initData.append(Point(rowFloatList))
     return initData
 
+# This function must be in Kmeans class. But... :))
+def get_last_centers(dirLastCenters: str, fileName: str, splitType: str) -> list:
+    lastCentersStr = read_lines_to_list(dirLastCenters, fileName, ', ')[:-1]
+    lastCenters = list()
+    for centerStr in lastCentersStr:
+        coordList = list()
+        for coordStr in centerStr:
+            coordList.append(float(coordStr))
+        lastCenters.append(Point(coordList))
+    return lastCenters
+
+def predict_new_point_temporary(lastCenters: list(), newPoint: Point) -> str:
+    res = str(0)
+    min = lastCenters[0].euclid_distance(newPoint)
+    for center in lastCenters:
+        d = center.euclid_distance(newPoint)
+        if min > d:
+            min = d
+            res = str(lastCenters.index(center))
+    return res
+
 def main():
     dir = './datasets/'
     datasetName = ['train_vn.txt', 'train_en.txt', 'Spend_Age.txt', 'BaiTapAnBinhLinhetc.txt']
     splitType = ' '
     fileName = datasetName[-1]
+    lastCentersDir = './outfile/' + fileName + '/'
 
     An = Point([10, 10])
     Binh = Point([40, 20])
@@ -47,20 +69,27 @@ def main():
     initData = list()
     initData = set_initData_with_nonLabeledData(dir, fileName, splitType)
     # initData = [Loc,Nhan,Nghia,An,Binh,Linh,Phat,Trong,Nhan1,Nhan2,Nhan3,Nhan4,Nhan5,Nhan6,Nhan7]
+    # k = 3
     # k = 2 # for train_vn
     # k = 13 # for train_en
-    k = 3 # spend_age
     dType = 1 # euclid distance
 
+    k = read_input_file_to_int(dir, 'k.txt')
+    
     if k > len(initData):
         print('k nhieu hon du lieu.')
         return
-    kmeans = Kmeans(initData, k, dType, fileName)
-    kmeans.initial_step()
-    kmeans.update_step()
+    # kmeans = Kmeans(initData, k, dType, fileName)
+    # kmeans.initial_step()
+    # kmeans.update_step()
+    # for cluster in kmeans.kCluster:
+    #     print(len(cluster))
+
+    lastCenters = get_last_centers(lastCentersDir, 'lastCenters.txt', ', ')
     
-    for cluster in kmeans.kCluster:
-        print(len(cluster))
+    newPoint = Point([20,80])
+    # newPoint = An
+    print('Predict {0} -> cluster {1}'.format(newPoint.display_with_parentheses(), predict_new_point_temporary(lastCenters, newPoint)) )
 
 
 if __name__=="__main__": main()
