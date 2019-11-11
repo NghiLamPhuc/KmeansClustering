b388cfc8400ad82b79a4d1e4326e688d2f904601
lamphucnghi313 <lamphucnghi@gmail.com>
2019-11-11 15:32:36 +0700

    Đọc k. :))

--- read_file.py
+++ read_file.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 # Hàm đọc file dạng nhiều dòng, mỗi dòng là transaction.
 # mỗi transaction chứa item.
-def read_input_file(link, fileName, splitType) -> dict:
+def read_input_file_to_dict(link, fileName, splitType) -> dict:
     f = open(link + fileName, 'r', encoding = 'utf-8')
     inpDict = defaultdict(dict)
     i = 0
@@ -13,7 +13,7 @@ def read_input_file(link, fileName, splitType) -> dict:
     return inpDict
 # Hàm đọc file dạng nhiều dòng, mỗi dòng là transaction.
 # Cột đầu tiên là tên transaction.
-def read_input_file_with_row_name(link, fileName, splitType) -> dict:
+def read_input_file_with_row_name_to_dict(link, fileName, splitType) -> dict:
     f = open(link + fileName, 'r', encoding = 'utf-8')
     inpDict = defaultdict(dict)
     for line in f:
@@ -40,3 +40,10 @@ def read_lines_to_list(link, fileName, splitType) -> list:
     [List.append(line.rstrip().split(splitType)) for line in f]
     f.close()
     return List
+
+def read_input_file_to_int(link, fileName) -> int:
+    num = 0
+    f = open(link + fileName, 'r', encoding = 'utf-8')
+    num = int(f.read())
+    f.close()
+    return num
