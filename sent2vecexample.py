9975172288a58098a4e2eb64f14da9eb98ce0b41
lamphucnghi313 <lamphucnghi@gmail.com>
2019-11-21 04:04:25 +0700

    no message

--- sent2vecexample.py
+++ sent2vecexample.py
@@ -9,11 +9,11 @@ fileDir = ['./TFIDF/', './datasets/']
 listRawSentence = read_file.read_line_to_sentenceList(fileDir[1], fileName[1] + '.txt', '\n')
 linkModel = './datasets/{0}2vec.model'.format(fileName[1])
 # # train
-# model = Word2Vec(listRawSentence, size=20, window=5, min_count=1)
-# model.save(linkModel)
+model = Word2Vec(listRawSentence, size=2, window=5, min_count=1)
+model.save(linkModel)
 # # loadmodel
-sent2vec = Sentence2Vec(linkModel)
-listVect = []
-for rawSent in listRawSentence:
-    listVect.append(sent2vec.get_vector(rawSent).tolist())
-write_file.list_to_txt(listVect, 'datasets', 'Sent2Vect.txt')
+# sent2vec = Sentence2Vec(linkModel)
+# listVect = []
+# for rawSent in listRawSentence:
+#     listVect.append(sent2vec.get_vector(rawSent).tolist())
+# write_file.list_to_txt(listVect, 'datasets', 'Sent2Vect.txt')
